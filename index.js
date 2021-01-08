75e9f1ff5d619e5be1497023a1c71c63ab6ccbb6
PriontoAbdullah <priontosoft@gmail.com>
2021-01-08 15:04:45 +0600

    Update all services with get & post req

--- index.js
+++ index.js
@@ -6,15 +6,56 @@ const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 
 const app = express();
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const port = 5000;
 
-app.get('/', (req, res) => {
-	res.send('Welcome to volunteer Network Server');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v9ypd.mongodb.net/${process.env
+	.DB_NAME}?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect((err) => {
+	const servicesCollection = client.db('volunteerNetwork').collection('services');
+	const eventsCollection = client.db('volunteerNetwork').collection('events');
+
+	console.log('Volunteer Network DataBase Connected');
+
+	// Added all services Information
+	app.post('/addAllServices', (req, res) => {
+		const mainData = req.body;
+		servicesCollection.insertMany(mainData).then((result) => {
+			console.log(result);
+			console.log(result.insertedCount, 'All Data Inserted');
+			res.send(result.insertedCount);
+		});
+	});
+
+	
+	// Get all services Information
+	app.get('/volunteerTasks', (req, res) => {
+		servicesCollection.find({}).toArray((err, documents) => {
+			res.send(documents);
+		});
+	});
+
+
+
+
+
+
+
+
+
 });
 
-app.listen(process.env.PORT || port);
+// Root Route
+app.get('/', (req, res) => {
+	res.send('Welcome to Volunteer Network Server');
+});
+
+// PORT
+app.listen(process.env.PORT || port, () => {
+	console.log('listening on port');
+});
